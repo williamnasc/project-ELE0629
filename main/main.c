eed38dbb32e76bdfe23facbad2d72bc25e550958
Enzohga <89107767+Enzohga@users.noreply.github.com>
2024-12-18 22:29:09 -0300

    Add files via upload

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
 
 //Includes FreeRTOS
 #include "freertos/FreeRTOS.h"
@@ -12,42 +13,39 @@
 #include "esp_random.h"
 #include "esp_err.h"
 #include "driver/gpio.h"
+#include <esp_system.h>
 
 //Biblioteca criada
 #include "wifi.h"
 #include "MQTT_lib.h"
 
 //Bibliotecas externas
-#include "mpu6050.h"
+#include <bmp180.h>
 #include <dht.h>
 
-#ifdef CONFIG_EXAMPLE_I2C_ADDRESS_LOW
-#define ADDR MPU6050_I2C_ADDRESS_LOW
-#else
-#define ADDR MPU6050_I2C_ADDRESS_HIGH
+#ifndef APP_CPU_NUM
+#define APP_CPU_NUM PRO_CPU_NUM
 #endif
 
 #define SENSOR_TYPE DHT_TYPE_DHT11
 
-#define DHT_GPIO 26
+#define DHT_GPIO 4
 
 QueueHandle_t queueTemperatura;
 QueueHandle_t queueUmidade;
 QueueHandle_t queuePressao;
-QueueHandle_t queueMPU;
 
 static const char *TAG = "MQTT Task";
 static const char *TAG_TEMPERATURA = "Temperatura Task";
 static const char *TAG_UMIDADE = "Umidade Task";
 static const char *TAG_PRESSAO = "Pressao Task";
-//static const char *TAG_MPU = "MPU Task";
 
 // Definição do struct 
 struct Dados { 
     char *texto; 
     float temperatura;
     float umidade; 
-    int pressao; 
+    uint32_t pressao; 
 };
 
 void dht11_task(void *pvParameters)
@@ -84,73 +82,34 @@ void dht11_task(void *pvParameters)
     }
 }
 
-
 void pressao_task(void *pvParameters)
 {
+    bmp180_dev_t dev;
+    memset(&dev, 0, sizeof(bmp180_dev_t));
+
+    ESP_ERROR_CHECK(bmp180_init_desc(&dev, 0, CONFIG_EXAMPLE_I2C_MASTER_SDA, CONFIG_EXAMPLE_I2C_MASTER_SCL));
+    ESP_ERROR_CHECK(bmp180_init(&dev));
+
     //inicia a fila que salva os valores de temperatura
     queuePressao = xQueueCreate(1, sizeof(uint32_t));
     
-    int count = 0;
-    while (1) {
-        count++;
-        // Envia o valor de 'count' para a fila
-        if (xQueueSend(queuePressao, &count, portMAX_DELAY) == pdPASS) {
-            ESP_LOGI(TAG_PRESSAO, "Enviado para a fila: %d", count);
+
+    while (1) 
+    {
+        float temp;
+        uint32_t pressure;
+
+        bmp180_measure(&dev, &temp, &pressure, BMP180_MODE_STANDARD);
+
+        if (xQueueSend(queuePressao, &pressure, portMAX_DELAY) == pdPASS) {
+            ESP_LOGI(TAG_PRESSAO, "Enviado para a fila: %"PRIu32"", pressure);
         } else {
             ESP_LOGE(TAG_PRESSAO, "Falha ao enviar para a fila");
         }
 
-        vTaskDelay(pdMS_TO_TICKS(1000));  // Aguarda 1 segundo
+        vTaskDelay(pdMS_TO_TICKS(2000));  // Aguarda 1 segundo
     }
 }
-
-/*void mpu6050_test(void *pvParameters)
-{
-    mpu6050_dev_t dev = { 0 };
-
-    ESP_ERROR_CHECK(mpu6050_init_desc(&dev, ADDR, 0, CONFIG_EXAMPLE_SDA_GPIO, CONFIG_EXAMPLE_SCL_GPIO));
-
-    while (1)
-    {
-        esp_err_t res = i2c_dev_probe(&dev.i2c_dev, I2C_DEV_WRITE);
-        if (res == ESP_OK)
-        {
-            ESP_LOGI(TAG_MPU, "Found MPU60x0 device");
-            break;
-        }
-        ESP_LOGE(TAG_MPU, "MPU60x0 not found");
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
-
-    ESP_ERROR_CHECK(mpu6050_init(&dev));
-
-    ESP_LOGI(TAG_MPU, "Accel range: %d", dev.ranges.accel);
-    ESP_LOGI(TAG_MPU, "Gyro range:  %d", dev.ranges.gyro);
-
-    queueMPU = xQueueCreate(1, sizeof("000.000, 000.000, 000.000"));
-
-    while (1)
-    {
-        float temp;
-        mpu6050_acceleration_t accel = { 0 };
-        mpu6050_rotation_t rotation = { 0 };
-
-        ESP_ERROR_CHECK(mpu6050_get_temperature(&dev, &temp));
-        ESP_ERROR_CHECK(mpu6050_get_motion(&dev, &accel, &rotation));
-
-        ESP_LOGI(TAG_MPU, "Acceleration: x=%.2f   y=%.2f   z=%.2f", accel.x, accel.y, accel.z);
-        
-        // JUNTA OS DADOS DO MPU NUMA STRING
-        char converted_value[50];
-        sprintf(converted_value, "(%.2f, %.2f, %.2f)", accel.x, accel.y, accel.z);        
-
-        if (xQueueSend(queueMPU, &converted_value, portMAX_DELAY) == pdPASS) {
-            ESP_LOGI(TAG_MPU, "Enviado para a fila: %s", converted_value);
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(1000));  // Aguarda 1 segundo
-    }
-}*/
 
 void mqtt_task(void *pvParameters)
 {
@@ -170,20 +129,12 @@ void mqtt_task(void *pvParameters)
 
    while(1){
         // INICIA A ESTRUTA QUE JUNTA OS DADOS
-        struct Dados pacote = {NULL, 1.0, 0.0, NULL};
+        struct Dados pacote = {NULL, 0.0, 0.0, 0.0};
 
        if(mqtt_connected()){
             
             // ATUALIZA STATUS
             mqtt_publish("ELE0629/Weather/Status", "ON", 0, 0);
-
-            // Espera receber um valor da fila de Temperatura
-            /*char received_mpu[50];
-            if (xQueueReceive(queueMPU, &received_mpu, portMAX_DELAY) == pdTRUE) {
-                ESP_LOGI(TAG_TEMPERATURA, "Recebido do MPU: %s",  received_mpu);
-                // pacote.texto = received_mpu;
-                mqtt_publish("ELE0629/Weather/MPU", received_mpu, 0, 0);   
-            }*/
 
             // Espera receber um valor da fila de Temperatura
             float received_temperatura;
@@ -197,28 +148,22 @@ void mqtt_task(void *pvParameters)
                 pacote.umidade = received_umidade;   
             }
 
-            int received_pressao;
+            uint32_t received_pressao;
             if (xQueueReceive(queuePressao, &received_pressao, portMAX_DELAY) == pdTRUE) {
                 pacote.pressao = received_pressao;   
             }
 
             
             //VERIFICA SE ESTA TODO MUNDO PREENCHIDO PRA ENVIAR
-            if (pacote.temperatura != 1.0 && pacote.umidade != 0.0 && pacote.pressao != NULL)
+            if (pacote.temperatura != 0.0 && pacote.umidade != 0.0 && pacote.pressao != 0.0)
             {
 
                 ESP_LOGI(TAG_TEMPERATURA, "Temperatura no pacote: %.2f",  pacote.temperatura);
-                // // CONVERTE O VALOR RECEBIDO PARA STRING
-                // char converted_temp[50];
-                // sprintf(converted_temp, "%d", pacote.temperatura);
-                // // PUBLICA NO BROKER
-                // mqtt_publish("ELE0629/Weather/Temperatura", converted_temp, 0, 0);
 
-                // JUNTA OS DADOS DO MPU NUMA STRING
                 char json_string[50];
                 sprintf(
                     json_string, 
-                    "{Temp: %.2f °C, Umi: %.2f, Press: %d}", 
+                    "{Temp: %.2f °C, Umi: %.2f, Press: %"PRIu32" Pa}", 
                     pacote.temperatura, pacote.umidade, pacote.pressao
                 );        
 
@@ -226,9 +171,9 @@ void mqtt_task(void *pvParameters)
 
                 // RESETA OS VALORES DO STRUT
                 pacote.texto = NULL;
-                pacote.temperatura = 1.0;
+                pacote.temperatura = 0.0;
                 pacote.umidade = 0.0;
-                pacote.pressao = NULL;
+                pacote.pressao = 0.0;
 
             }
        }
@@ -243,9 +188,8 @@ void app_main(void)
     // task
     ESP_ERROR_CHECK(i2cdev_init());
 
-    //xTaskCreate(mpu6050_test, "mpu6050_test", configMINIMAL_STACK_SIZE * 6, NULL, 2, NULL);
     xTaskCreate(dht11_task, "dht11_task", configMINIMAL_STACK_SIZE * 6, NULL, 2, NULL);
-    xTaskCreate(pressao_task, "pressao_task", configMINIMAL_STACK_SIZE * 6, NULL, 2, NULL);
+    xTaskCreatePinnedToCore(pressao_task, "pressao_task", configMINIMAL_STACK_SIZE * 6, NULL, 2, NULL, APP_CPU_NUM);
     xTaskCreate(mqtt_task, "mqtt_task", configMINIMAL_STACK_SIZE * 6, NULL, 2, NULL);
 
     vTaskDelay(5000/portTICK_PERIOD_MS);
